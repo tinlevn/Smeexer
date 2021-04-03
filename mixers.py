7b4eca4196ac6fdf4695baa7ebfea21e2bbd4a9d
Tin Le VN <thanhtin.le.vn@gmail.com>
2021-04-03 14:47:20 -0500

    Code cleanup and logical fixes

--- mixers.py
+++ mixers.py
@@ -4,7 +4,6 @@ from random import randint
 my_file = open("bip0039.txt", "r")
 content = my_file.read()
 
-
 """
 Dicts and sets have an O(1)
 time complexity for membership checking because 
@@ -18,7 +17,7 @@ mnemonic_set = set(mnemonic_list)
 
 
 def seed_validation(seed_list):
-    if len(seed_list) not in (12,18,24):
+    if len(seed_list) not in (12, 18, 24):
         print("Please enter a valid seed-phrase set")
         return False
     for i in seed_list:
@@ -28,22 +27,22 @@ def seed_validation(seed_list):
 
 
 def print_facts():
-    print("All the possibilities of a 12-word seed phrase using the BIP-0039 standard are: %d" % math.comb(2048, 12))
-    print("All the possibilities of a 24-word seed phrase using the BIP-0039 standard are: %d" % math.comb(2048, 24))
+    print("Can someone guess my seed? There are 2^256 different possible 24-word mnemonic seeds. "
+          "For comparison, the number of atoms on Earth is estimated to be around 2^166.\n "
+          "Just let that sink in for a second. "
+          "The chance of someone else being able to guess your seed is astronomically small, to say the least.")
 
 
 def stepping_stone_mix(seed_list):
     set_length = len(seed_list)
     stoneMix = seed_list.copy()
 
+    # Swapping elements
     for i in range(0, set_length, 2):
-        temp = stoneMix[i]
-        stoneMix[i] = stoneMix[i + 1]
-        stoneMix[i + 1] = temp
+        stoneMix[i], stoneMix[i + 1] = stoneMix[i + 1], stoneMix[i]
+    # One last swap
+    stoneMix[-1], stoneMix[0] = stoneMix[0], stoneMix[-1]
 
-    temp = stoneMix[-1]
-    stoneMix[-1] = stoneMix[0]
-    stoneMix[0] = temp
     return stoneMix
 
 
@@ -51,16 +50,15 @@ def odd_even(seed_list):
     set_length = len(seed_list)
     oddEvenMix = seed_list.copy()
 
+    # Swap even positions
     for i in range(2, set_length, 4):
         if i % 2 == 0:
-            temp = oddEvenMix[i]
-            oddEvenMix[i] = oddEvenMix[i - 2]
-            oddEvenMix[i - 2] = temp
+            oddEvenMix[i], oddEvenMix[i - 2] = oddEvenMix[i - 2], oddEvenMix[i]
+    # Swap odd positions
     for i in range(3, set_length, 4):
         if i % 2 != 0:
-            temp = oddEvenMix[i]
-            oddEvenMix[i] = oddEvenMix[i - 2]
-            oddEvenMix[i - 2] = temp
+            oddEvenMix[i], oddEvenMix[i - 2] = oddEvenMix[i - 2], oddEvenMix[i]
+
     return oddEvenMix
 
 
@@ -118,6 +116,27 @@ def odd_one_out_mix(long_seed, side):
         print("Please enter a valid side operation:\n"
               "Hint:l, L, left, LEFT, Left for left shift\n"
               "r,R,right,RIGHT,Right for right shift")
+
+
+def onion_ring(seed_list, layer):
+    set_length = len(seed_list)
+    onion = seed_list.copy()
+
+    # Only need to iterate half the length to
+    # swap all elements
+    ring_size = set_length // 2
+
+    if layer in {"out", "OUT", "o", "O"}:
+        start = 0
+    elif layer in {"in", "IN", "i", "I"}:
+        start = 1
+    else:
+        print("Please choose in(IN,i,I) or\
+              out(OUT,o,O)")
+    # Swap loop
+    for i in range(start, ring_size, 2):
+        onion[i], onion[-i - 1] = onion[-i - 1], onion[i]
+    return onion
 
 
 def new_input():
